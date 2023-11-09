570e7921d3f135421bb58acf2014ea786ba9d3eb
anuragpampati <48316130+anuragpampati@users.noreply.github.com>
2023-11-08 19:25:13 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,73 @@
+
+
+
+
+for(var i=0;i< document.querySelectorAll(".drum").length;i++){
+    document.querySelectorAll("button")[i].addEventListener("click",function(){
+        var p  = this.innerHTML;
+        makesound(p);
+            if (p == 'w'){
+                clickf("tom-1.mp3");
+            } 
+            if (p == 'a'){
+                clickf("tom-2.mp3");
+            } 
+            if (p == 's'){
+                clickf("tom-3.mp3");
+            } 
+            if (p == 'd'){
+                clickf("tom-4.mp3");
+            } 
+            if (p == 'j'){
+                clickf("crash.mp3");
+            } 
+            if (p == 'k'){
+                clickf("snare.mp3");
+            } 
+            if (p == 'l'){
+                clickf("kick-bass.mp3");
+            } 
+    });
+}
+
+document.addEventListener("keypress" , function(event){
+    makesound(event.key);
+});
+
+function makesound(key){
+
+    if (key == 'w'){
+        clickf("tom-1.mp3");
+    } 
+    if (key == 'a'){
+        clickf("tom-2.mp3");
+    } 
+    if (key == 's'){
+        clickf("tom-3.mp3");
+    } 
+    if (key == 'd'){
+        clickf("tom-4.mp3");
+    } 
+    if (key == 'j'){
+        clickf("crash.mp3");
+    } 
+    if (key == 'k'){
+        clickf("snare.mp3");
+    } 
+    if (key == 'l'){
+        clickf("kick-bass.mp3");
+    }
+
+}
+
+
+function clickf(st){
+    console.log(st);
+    var k = "./sounds/" + st;
+    var s = new Audio(k);
+    s.play();
+}
+
+
+
+
